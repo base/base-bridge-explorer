36a132efe62d9914b87e26dca75221c889bc127d
Jack Chuma <jack.chuma@coinbase.com>
2025-10-07 18:03:00 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
 
         {/* content */}
         <div className="relative z-10">{children}</div>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
